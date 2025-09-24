da69c7cf434d28157f6d3013c4f737963c3157cf
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-09-24 14:42:01 +0200

    en ny uppgift påbörjas

--- main.js
+++ main.js
@@ -7,15 +7,15 @@
 //  for odd numbers.
 
 
-(Tänker vi likadant?)
-Pseudokod:
+// (Tänker vi likadant?)
+// Pseudokod:
 
-Mål: skapa en funktion som svarar om ett tal är jämnt eller udda
-Input: ett heltal
-output: en string
+// Mål: skapa en funktion som svarar om ett tal är jämnt eller udda
+// Input: ett heltal
+// output: en string
 
-STEG 1
-Testar om input går att dela i två lika stora delar, om det går ska output vara "even" och om det inte går ska det vara "odd".
+// STEG 1
+// Testar om input går att dela i två lika stora delar, om det går jämt upp i två delar ska  output vara "even" och om det inte går ska det vara "odd".
 
 
 
